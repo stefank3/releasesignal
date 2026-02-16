415f44d6f10f7db91c5de0fb874df7bb1bb8ae52
Stefan.Kajchevski <K4jc3vski1!>
2026-02-16 12:16:05 +0100

    Fix Vercel build: move generated Prisma client under lib/

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,5 @@
 import "server-only";
-import { PrismaClient } from "../generated/prisma/client";
+import { PrismaClient } from "./generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from "pg";
 
